4b1f6dceae7dd548bf01c464afd3c24db92f14d7
Aman Aditya Pandey <43087916+AAP-33@users.noreply.github.com>
2022-08-25 23:20:44 +0530

    Time: 2 ms (74.62%), Space: 75 MB (33.23%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -2,25 +2,43 @@ class Solution {
     public int maxSubArray(int[] arr) {
         
 
+//         int sum=0;
+//         int max_sum=Integer.MIN_VALUE;
+
+//         for(int i=0;i<arr.length;i++){
+
+//             sum=sum+arr[i];
+
+//   //          check condition
+
+//             if(sum > max_sum){
+//             max_sum=sum;
+//             }
+
+// //            * check another condition if sum is less than 0 update the sum with zero;
+
+//             if(sum<0){
+//              sum=0;
+//             }
+//         }
+//         return max_sum;
+        
+        //MY KADANE's method
+        
+        
         int sum=0;
-        int max_sum=Integer.MIN_VALUE;
-
-        for(int i=0;i<arr.length;i++){
-
-            sum=sum+arr[i];
-
-  //          check condition
-
-            if(sum > max_sum){
-            max_sum=sum;
+        int bs=Integer.MIN_VALUE; //bs-->best sum
+        for(int ele:arr){
+            
+            sum+=ele;
+            if(sum>bs){
+                bs=sum;
+                //yaha chahe to hum do index pointers leke uss subarray ko bhi kahi store kar sakte the
+                
             }
-
-//            * check another condition if sum is less than 0 update the sum with zero;
-
-            if(sum<0){
-             sum=0;
-            }
+            
+            if(sum<0) sum=0;
         }
-        return max_sum;
+        return bs;
     }
 }
