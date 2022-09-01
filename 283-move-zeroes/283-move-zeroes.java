fd6184c922cf3a2279dda2e583d01d945f70af91
Aman Aditya Pandey <43087916+AAP-33@users.noreply.github.com>
2022-09-01 14:59:34 +0530

    Time: 5 ms (15.96%), Space: 54.8 MB (53.02%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -4,6 +4,8 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
                 nums[j++]=nums[i];
+                // j++;
+                // nums[i]=0;
             }
         }
         for(int i=j;i<nums.length;i++){
