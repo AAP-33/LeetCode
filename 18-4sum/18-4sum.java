b08f7d78642847682a47144b13945e7374018cf0
Aman Aditya Pandey <43087916+AAP-33@users.noreply.github.com>
2022-09-02 11:47:04 +0530

    Time: 37 ms (41.18%), Space: 44.5 MB (64.01%) - LeetHub

---
+++ 18-4sum/18-4sum.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        
+        List<List<Integer>> res=new ArrayList<List<Integer>>();
+        
+        if(nums==null || nums.length==0 ||nums[0]==1000000000 || nums[0]==-1000000000 )
+            return res;
+        Arrays.sort(nums);
+        
+        int n=nums.length;
+        
+        for(int i=0;i<n-3;i++)
+        {
+            for(int j=i+1;j<n-2;j++){
+                long target_2=target-nums[i]-nums[j];
+                
+                int front=j+1,back=n-1;
+                
+                while(front<back){
+                    int two_sum=nums[front]+nums[back];
+                    if(two_sum<target_2)
+                    {
+                        front++;
+                    }
+                    else if(two_sum>target_2){
+                        back--;
+                    }
+                    else{
+                        List<Integer> quad=new ArrayList<>();
+                        quad.add(nums[i]);
+                        quad.add(nums[j]);
+                        quad.add(nums[front]);
+                        quad.add(nums[back]);
+                        
+                        res.add(quad);
+                        
+                        while(front<back && nums[front]==quad.get(2)) front++;
+                        while(front<back && nums[back]==quad.get(3)) back--;
+                    }
+                }
+                //skip duplicates of number 2 ,means no. nums[j]
+                while(j+1<n && nums[j+1]==nums[j]) j++;
+            }
+                while(i+1<n && nums[i+1]==nums[i]) i++;
+        }
+        
+        
+        
+        
+        return res;
+    }
+}
